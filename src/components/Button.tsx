d30caf6ffc75b7fe9518af34cfc468d1348287ac
4kbr <igyutubfesbuk@gmail.com>
2023-11-25 22:37:28 +0700

    react typescript bank hoo

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+interface _Props {
+  styles?: string;
+}
+const Button = ({ styles }: _Props) => (
+  <button
+    type="button"
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+  >
+    Get Started
+  </button>
+);
+
+export default Button;
